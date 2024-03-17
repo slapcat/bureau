893a764661e31fa0824172426d888f3179c0965e
Jake Nabasny <jake@nabasny.com>
2024-03-17 17:06:22 -0400

    adding start of keepalived schemas

--- generator.go
+++ generator.go
@@ -1,30 +1,35 @@
 package main
 
 import "os"
-
+import "log"
 func GenerateDefault(path string, data string) error {
 	f, err := os.Create(path)
-
 	if err != nil {
-
-
 		return err
-
 	}
 
 	_, err = f.WriteString(data)
-
+	defer f.Close()
 	if err != nil {
-
-
-        f.Close()
-
 		return err
+	}
 
+	return nil
+}
+
+func GenerateKeepalived(path string, data string) error {
+	f, err := os.Create(path)
+	if err != nil {
+		return err
 	}
 
 
-        f.Close()
+	//unpack struct
+	_, err = f.WriteString(data)
+	defer f.Close()
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
